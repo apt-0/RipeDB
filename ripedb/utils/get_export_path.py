dc164d9d70b63e0be47abf43c8781fa31bde02ce
Nick <niccolopenzo@gmail.com>
2024-04-01 20:59:46 +0200

    Update get_export_path.py

--- ripedb/utils/get_export_path.py
+++ ripedb/utils/get_export_path.py
@@ -1,21 +1,26 @@
 import os
 
-
-def get_export_path(prompt):
+def get_export_path(prompt, default_path=None):
     """
-    Prompt the user to enter an export path.
+    Prompt the user to enter an export path or use the default one.
 
     Args:
         prompt (str): The message to display to the user.
+        default_path (str, optional): A default path to use if provided. Defaults to None.
 
     Returns:
-        str: The export path chosen by the user or the current directory.
+        str: The export path chosen by the user, the provided default path, or the current directory.
     """
+    if default_path and os.path.exists(default_path):
+        return default_path
+    elif default_path and not os.path.exists(default_path):
+        print(f"The specified directory does not exist: {default_path}")
+        return os.getcwd() 
+    
     export_path = input(prompt)
     if not export_path:
-        export_path = os.getcwd()
-    else:
-        if not os.path.exists(export_path):
-            print(f"The specified directory does not exist: {export_path}")
-            return get_export_path(prompt)
+        return os.getcwd() 
+    elif not os.path.exists(export_path):
+        print(f"The specified directory does not exist: {export_path}")
+        return get_export_path(prompt) 
     return export_path
