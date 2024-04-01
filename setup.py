db1b58c5aac3b538ea09a56edc5eb95ca10aa6d0
Nick <niccolopenzo@gmail.com>
2024-04-01 22:04:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="RipeDB",
-    version="0.3.6",
+    version="0.4.0",
     packages=find_packages(),
     install_requires=[
         'requests',
